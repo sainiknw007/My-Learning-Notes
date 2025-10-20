e5415104fc310a833131c2b2a79005ed328f4742
Akhilesh Saini <146843314+sainiknw007@users.noreply.github.com>
2025-10-20 06:28:03 +0530

    Create calculation.sh

---
+++ Learning-Linux-Basics-Notes/Quiz-Shell-Scripting/calculation.sh
@@ -0,0 +1,12 @@
+# shell script to print the Sum, Difference, Product and Quotient of these values.
+
+A=20
+B=10
+
+echo "Sum is $(( A + B))"
+
+echo "Difference is $(( A - B))"
+
+echo "Product is $(( A * B))"
+
+echo "Quotient is $(( A / B))"
